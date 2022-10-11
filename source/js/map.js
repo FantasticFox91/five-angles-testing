bc9947ed137a68f5cdb7a4dff5dbaed250effec2
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2022-10-11 16:50:22 +0300

    feat: implement map

---
+++ source/js/map.js
@@ -0,0 +1,25 @@
+const map = document.querySelector('#map');
+const adressInput = document.querySelector('.form__input--textarea');
+let myMap;
+
+function init(){
+  myMap = new ymaps.Map(map, {
+      center: [55.76, 37.64],
+      zoom: 10,
+  });
+}
+
+map.innerHTML = "";
+ymaps.ready(init);
+
+adressInput.addEventListener('change', () => {
+  if (adressInput.value) {
+    myMap.geoObjects.removeAll();
+    let myGeocoder = ymaps.geocode(`Россия, ${adressInput.value}`);
+    myGeocoder.then(function(res) {
+        myMap.geoObjects.add(res.geoObjects);
+        myMap.setCenter(res.geoObjects.get(0).geometry.getCoordinates());
+        myMap.setZoom(17)
+    });
+  }
+})
